740fd5cb8b6efe77402cc9201f7253d8c0a08e2a
Erick Villalobos <villalobose143@gmail.com>
2024-06-19 15:10:40 -0500

    fix: habilitando page

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import Home from "../layout/Home";
+import Home from "../layout/Home";
 import Pdf from "../container/Pdf";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,12 +13,12 @@ const App = () => {
   return (
     <AppContext.Provider value={initialState}>
       <BrowserRouter>
-          <Routes>
-            {/* <Route exact path="/" element={<Home />} /> */}
-            <Route exact path="/" element={<NotFound />} />
-            <Route path="/CV_VillalobosCasanatanErick.pdf" element={<Pdf />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {/* <Route exact path="/" element={<NotFound />} /> */}
+          <Route path="/CV_VillalobosCasanatanErick.pdf" element={<Pdf />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </AppContext.Provider>
   );
